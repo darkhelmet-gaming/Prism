95cf85be41a2b2dfcaccf7295e45d6277513e051
Mike Botsko <botsko@gmail.com>
2016-01-23 17:11:11 -0800

    Cancel event when wand active

--- src/main/java/com/helion3/prism/events/listeners/RequiredInteractListener.java
+++ src/main/java/com/helion3/prism/events/listeners/RequiredInteractListener.java
@@ -70,6 +70,8 @@ public class RequiredInteractListener {
 
                 // Pass off to an async lookup helper
                 AsyncUtil.lookup(session);
+
+                event.setCancelled(true);
             }
         }
     }
